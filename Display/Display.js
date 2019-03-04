5fd9fa3c86124e7d5e38f6ba1eeee520a6c7c8da
atrinh3 <trinh0148@gmail.com>
2019-03-03 17:39:15 -0800

    Adding .js file.

---
+++ Display/Display.js
@@ -0,0 +1,66 @@
+var filename = "order_list.csv";
+var orders = 0;
+var columns = 0;
+
+function preload() {
+    table = loadTable(filename, 'csv');
+}
+
+
+function setup() {
+    createCanvas(displayWidth, displayHeight);
+    //fullscreen(createCanvas(displayWidth, displayHeight));
+    columns = table.getColumnCount();
+    //frameRate(30);
+}
+
+
+function draw() {
+    background(220);
+    //updateTableData();
+    showTable();
+}
+
+
+function updateTableData() {
+    table = table.loadTable(filename, 'csv', loadTable_cb(), _loadTable_cb());
+    orders = table.getRowCount();
+}
+
+function loadTable_cb() {
+    print("Table reload success.");
+}
+
+function _loadTable_cb() {
+    print("Table reload fail.");
+}
+
+function showTable() {
+    let rowHeight = displayHeight * 0.09;
+    let columnWidth = displayWidth * 0.2;
+    let startX = displayWidth * 0.05;
+    let startY = displayHeight * 0.05;
+    let i = 0;
+    let j = 0;
+    orders = table.getRowCount();
+    columns = table.getColumnCount();
+    showHeader(startX, startY, columnWidth, rowHeight);
+    textSize(20);
+    for (i = 0; i < orders; i++) {
+        for (j = 0; j < columns; j++) {  
+            text(table.get(i, j),
+                startX + (columnWidth * (j)),
+                startY + (rowHeight * (i + 1)),
+                rowHeight,
+                columnWidth);
+        }
+    }
+}
+
+function showHeader(x, y, width, height) {
+    textSize(30);
+    text("Order Number", x, y, width, height);
+    text("Name", x + width, y, width, height);
+    text("Order", x + width * 2, y, width, height);
+    text("Status", x + width * 3, y, width, height);
+}

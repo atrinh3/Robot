0131a2a8323be94bfe10487f0284f39f702d121f
atrinh3 <trinh0148@gmail.com>
2019-03-05 22:17:12 -0800

    Initial commit

---
+++ Display/Display.js
@@ -0,0 +1,32 @@
+/* ************************************************************ *
+ * This will be the code that controls the display that will be *
+ * mounted on BobaKat.                                          *
+ * ************************************************************ */
+
+
+// Could make a CSV file to "randomly" vary messages in a controlled
+// way.
+// ie. Want to say "hello" -> look in the "hello" column/row -> pick
+//     anything from that category and display the message.
+function preload() {
+
+}
+
+
+function setup() {
+    createCanvas(displayWidth, displayHeight);
+    //fullscreen(createCanvas(displayWidth, displayHeight));
+}
+
+
+function draw() {
+    background(220);
+}
+
+
+// Start Order -> "Hello"
+// Steps
+// Finish Order -> "Thank you" / "Bye"
+function saySomething() {
+
+}

b41800081822c1a45ae081d91f221e5afb0d9277
atrinh3 <36494242+atrinh3@users.noreply.github.com>
2019-06-15 11:25:31 -0700

    Adding some more message and statusing features

--- LiquidDispenser/ArduinoCode.ino
+++ LiquidDispenser/ArduinoCode.ino
@@ -6,21 +6,22 @@ Since the Arduino can only output a 5V signal, the Arduino will
 require a connection to a 5V relay.  A 12V supply will be connected
 to the ends of the relay to be switched on/off by the 5V signal.
 
-TODO:
+TODO 6/15:
     Required:
     - DONE - Receive a serial signal to commnad valve actuation
     - DONE - Output 5V to relay board to open solenoid valve
     - DONE - Stop 5V output to close solenoid valve
     
     Optional:
-    - Send serial signal indicating solenoid is open or closed
-    - Use a 2nd relay to or low voltage LED to indicate valve status 
+    - DONE - Send serial signal indicating solenoid is open or closed
+    - DONE - Use a 2nd relay to or low voltage LED to indicate valve status 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define SOLENOID_RELAY 2
+#define SOLENOID_LED 3
 
 char character;
 int charsAvailable;
@@ -29,6 +30,7 @@ String currentCommand, commandBuffer, serialInput;
 
 void setup() {
     pinMode(SOLENOID_RELAY, OUTPUT);
+    pinMode(SOLENOID_LED, OUTPUT);
     Serial.begin(9600);
 }
 
@@ -40,10 +42,12 @@ void loop() {
     
     if (serialInput.indexOf("Open valve") > 0) {
         openSolenoid(SOLENOID_RELAY);
+        ledOn(SOLENOID_LED);
     }
 
     if (serialInput.indexOf("Close valve") > 0) {
         closeSolenoid(SOLENOID_RELAY);
+        ledOff(SOLENOID_LED);
     }
 }
 
@@ -57,6 +61,16 @@ void openSolenoid(int controlPin) {
 void closeSolenoid(int controlPin) {
     digitalWrite(controlPin, LOW);
     Serial.println("Valve closed");
+}
+
+
+void ledOn(int ledPin) {
+    digitalWrite(ledPin, HIGH);
+}
+
+
+void ledOff(int ledPin) {
+    digitalWrite(ledPin, LOW);
 }
 
 
